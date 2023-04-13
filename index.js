42d9a4d8b914219873aac7b5aa3fcc2014ba991f
Daniel Liu <daniel.albert.liu@gmail.com>
2023-04-12 22:11:18 -0500

    Completed 3.3-3.4

--- index.js
+++ index.js
@@ -38,6 +38,24 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${count} people</p><p>${time}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+  response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
